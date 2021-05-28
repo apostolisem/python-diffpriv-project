2402296eb96c88a78040d800e4fc990ca66ff55b
Apostolis Em <59648316+apostolisem@users.noreply.github.com>
2021-05-29 01:14:48 +0200

    source code improved

--- differential_privacy_project.py
+++ differential_privacy_project.py
@@ -21,9 +21,9 @@ def Exponential_Mechanism(x, eps, hierarchy):
     return x
 
 # function to get the MSE for a privatized dataset
-def get_MSE(original_df, new_df, eps):
-    original_df = pd.Series(original_votes_df['Age'])
-    new_df = pd.Series(new_df['Age'].apply(Geometric_Mechanism, args=(eps,)))
+def get_MSE(original_df, eps):
+    original_df = pd.Series(original_df['Age'])
+    new_df = pd.Series(original_df.apply(Geometric_Mechanism, args=(eps,))).copy()
     difference = original_df.add(-1*new_df, fill_value=0)
     MSE = difference.apply(lambda x: x**2).mean()
     return MSE
@@ -33,41 +33,31 @@ original_votes_df = pd.read_csv('dataset.txt', sep=',').sample(frac=1).reset_ind
 privatized_votes_df = original_votes_df.copy()
 cities = pd.read_csv('data/ZipCodesGR.csv', sep=',', dtype=str).values.tolist()
 
-# anonymize the data - apply Geometric mechanism to age column
+# anonymize the data
 epsilon = 0.5
-privatized_votes_df['Age'] = privatized_votes_df['Age'].apply(Geometric_Mechanism, args=(epsilon,))
+privatized_votes_df['Age'] = privatized_votes_df['Age'].apply(Geometric_Mechanism, args=(epsilon,)) # apply Geometric mechanism to age column
+privatized_votes_df['Location'] = privatized_votes_df['Location'].apply(Exponential_Mechanism, args=(epsilon,cities)) # apply Exponential Hierarchical Mechanism to Location data
+privatized_votes_df = privatized_votes_df.drop(columns=['Name', 'Mobile']) # remove name and mobile columns
 
-# anonymize the data - apply Exponential Hierarchical Mechanism to Location data
-privatized_votes_df['Location'] = privatized_votes_df['Location'].apply(Exponential_Mechanism, args=(epsilon,cities))
-
-# anonymize the data - remove name and mobile columns
-privatized_votes_df = privatized_votes_df.drop(columns=['Name', 'Mobile'])
-
-# generate sample plots - votes by country
+# generate sample plots
 votes_by_country = original_votes_df['VoteCountry'].value_counts().head(10)
-votes_by_country.plot.bar()
-
-# generate sample plots - votes by age
+votes_by_country.plot.bar() # plot votes by country
 votes_by_age = original_votes_df[['Age','VoteCountry']]
-votes_by_age.plot.hist()
-
-# generate sample plots - votes by age from anonymized data
+votes_by_age.plot.hist() # plot votes by age
 votes_by_age_priv = privatized_votes_df[['Age','VoteCountry']]
-votes_by_age_priv.plot.hist()
+votes_by_age_priv.plot.hist() # votes by age from anonymized data
 
-# count values and difference
+# compute MSE
+MSE = get_MSE(original_votes_df, epsilon)
+
+# compute sensitivity between neighboring datasets
 original_values = original_votes_df["Age"].value_counts()
 privatized_values = privatized_votes_df["Age"].value_counts()
 difference = original_values.add(-1*privatized_values, fill_value=0)
-
-# compute MSE for privatized dataset
-MSE = get_MSE(original_votes_df, privatized_votes_df, epsilon)
-
-# compute sensitivity between neighboring datasets
 sensitivity = pd.DataFrame(abs(difference)).max()
 
 # output our results
 print(original_votes_df, privatized_votes_df)
 print("The sensitivity is:", sensitivity.Age)
-print("The MSE is:", MSE) # lower the better
+print("The MSE is:", MSE) # larger than 0
 plt.show()
